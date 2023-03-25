163a060837d333967be04132576e94424c0f821b
yassine14 <yassinedahmouch1@gmail.com>
2023-03-25 19:16:59 +0000

    project setup

---
+++ src/main/java/com/yd/kata/sp/SupermarketPricer.java
@@ -0,0 +1,13 @@
+package com.yd.kata.sp;
+
+/**
+ * Hello world!
+ *
+ */
+public class SupermarketPricer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
