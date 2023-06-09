7991c81fda943078c9e64d4ef1420766866d52f3
yassine14 <yassinedahmouch1@gmail.com>
2023-06-09 06:17:44 +0200

    Add java doc to the application entry point

--- src/main/java/com/yd/supermarket/kata/App.java
+++ src/main/java/com/yd/supermarket/kata/App.java
@@ -3,6 +3,12 @@ package com.yd.supermarket.kata;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This class represent the application entry point.
+ * 
+ * @author Yassine
+ *
+ */
 public class App {
 
     private static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
