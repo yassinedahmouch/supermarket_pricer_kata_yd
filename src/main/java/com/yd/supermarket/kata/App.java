397a39a8023a974586262a701b04ab9345211df8
yassine14 <yassinedahmouch1@gmail.com>
2023-06-09 06:16:26 +0200

    Add the application entry point

---
+++ src/main/java/com/yd/supermarket/kata/App.java
@@ -0,0 +1,13 @@
+package com.yd.supermarket.kata;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+
+    private static final Logger LOG = Logger.getLogger(App.class.getCanonicalName());
+
+    public static void main(String[] args) {
+        LOG.log(Level.INFO, "Hello world");
+    }
+}
